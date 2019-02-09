e46af9839ffe3e0aae11ea4120f9784c296b6c69
eliq1986 <elijahmquesada@gmail>
2019-02-08 20:41:27 -0800

    Moved array into index.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,55 +3,11 @@ import './App.css';
 
 import QuoteBox from './QuoteBox';
 
-const quotes = [
-
-  {
-    quote: "Is Butter a carb",
-    source: "Regina George",
-    year: 2004,
-    citation: "Mean Girls"
-
-
-  }, {
-    quote: `To see the world, things dangerous to come to, to see behind walls, draw closer, to find each other, and to feel. That is the purpose of life`,
-    source: "Walter Mitty",
-    year: 2013,
-    citation: "The Secret Life Of Walter Mitty"
-
-
-  }, {
-    quote: `What we do in life echoes in eternity.`,
-    source: "Maximus",
-    year: 2000,
-    citation: "Gladiator"
-
-
-  }, {
-    quote: `Kid, there are heroes and there are legends. Heroes get remembered, but legends never die. Follow your heart, kid, and you’ll never go wrong.`,
-    source: "Babe Ruth",
-    year: 1993,
-    citation: "The Sandlot"
-
-
-  }, {
-    quote: `Do, or do not. There is no “try`,
-    source: "Yoda",
-    year: 1980,
-    citation: "Empire Strikes Back"
-
-
-  }, {
-    quote: `Who is your daddy and what does he do`,
-    source: "John Kimble",
-    year: 1990,
-    citation: "Kindergarten Cop"
-  }
-];
 
 class App extends Component {
 
 state = {
-  quoteObj: quotes[Math.floor(Math.random() * quotes.length)]
+  quoteObj: this.props.quotes[Math.floor(Math.random() * this.props.quotes.length)]
 }
 
 
@@ -71,11 +27,10 @@ changeBackground = () => {
 
 render() {
 
-
   return (
     <div className="container" >
       <QuoteBox quote={this.state.quoteObj} />
-        <button onClick={()=>this.getRandomQuote(quotes)} id="loadQuote">Show another quote</button>
+        <button onClick={()=>this.getRandomQuote(this.props.quotes)} id="loadQuote">Show another quote</button>
     </div>
   );
 };
